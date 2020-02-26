56ee716e5c4af1fb4667a3705b6943e3b220789e
Marcus Ingelborn <mringelborn@gmail.com>
2020-02-26 13:32:46 +0100

    Added functionality to accept or reject applicant

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-
+const {Validator} = require('node-input-validator');
 
 /* Home AUTH: JWT */
 function CreateRoute(ctrl) {
@@ -25,12 +25,35 @@ function CreateRoute(ctrl) {
       });
   });
   
-  router.get('/SetApplicant/',
+  router.post('/SetApplicant/',
     passport.authenticate('jwt', {session: false}),
     (req, res) => {
       if (req.user.ROLE_ID == '1') {
-        ctrl.SetApplicant(x => {
-          res.send(x);
+        const validator = new Validator(req.body,
+          {
+            userId: 'required|integer',
+            accept: 'required|boolean',
+          }
+        );
+        validator.check().then((matched) => {
+          if (!matched) {
+            res.status(422).send(v.errors);
+          }
+          else {
+            res.send({status: true});
+            console.log(req.body);
+            const state = req.body.accept == 'true' ? 2 : 3;
+
+            ctrl.SetApplicant(
+              {
+                id: req.body.userId,
+                state: state,
+              },
+              x => {
+                res.send(x);
+              }
+            );
+          }
         });
       } else {
         res.send('Permission denied');
